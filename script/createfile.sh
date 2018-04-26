446a2888cb2c95fcdc41c419c9c97d0cb9b6a6ff
bahtiyur <widaya.bahtiar@gmail.com>
2018-04-26 16:18:47 +0700

    my first bash

---
+++ script/createfile.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+today=`date +%A`
+filename="today"
+        echo -n "Please enter your number : "
+        read x
+        touch "$today$x.sh"
+        echo  "Your filename is         : $today$x"
+
+result="$today$x.sh"
+        chmod a+rwx "$result"
+        echo  "#!/bin/bash" | sudo tee -a "$result" > /dev/null
+        echo  "================ added 1 file with number $x ================="
+
+
+
